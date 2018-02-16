b7d7a6c68d303d97fb8e3116e74859f7938efc16
lukedsmalley <lukedsmalley@gmail.com>
2018-02-16 01:22:22 -0500

    Integrated pigpio for relay control

--- app.js
+++ app.js
@@ -1,27 +1,88 @@
+
+const Gpio = require('pigpio').Gpio
 const express = require('express')
+
+const Direction = {
+    IN: false,
+    OUT: true
+}
+
 const app = express()
 
-const MASTER_POWER = "masterPower";
-const DIRECTION = "direction";
-const LEFT_WINCH = "leftWinch";
-const RIGHT_WINCH = "rightWinch";
-const MURRAY = "murray";
 
-app.get('/api', (req, res) => res.send('Hello from MCP!'))
+function SingleControlPin(inPin) {
+    var pin = new Gpio(inPin, { mode: Gpio.OUTPUT })
 
-app.get('/api/controls', function(req, res){
-	var controlName = req.query.control;
-	var state = req.query.state;
-	console.log('MCP control %o setting to ', controlName, state);
- 	
- 	// Plug In Control firing logic here
-	// Right now simply echoing back state we got
- 	res.send(controlName+': ' + state);
- 	
-});
+    this.direction = Direction.IN
+    
+    this.set = function() {
+        pin.digitalWrite(1)
+    }
 
-app.use(express.static('public', {
-  index: "index.html"
-}))
+    this.reset = function() {
+        pin.digitalWrite(0)
+    }
+}
+
+function DualControlPin(inPin, outPin) {
+    var inGpio = new Gpio(inPin, { mode: Gpio.OUTPUT })
+    var outGpio = new Gpio(outPin, { mode: Gpio.OUTPUT })
+
+    this.direction = Direction.IN
+    
+    this.set = function() {
+        if (this.direction == Direction.IN) {
+            inGpio.digitalWrite(1)
+            outGpio.digitalWrite(0)
+        } else {
+            inGpio.digitalWrite(0)
+            outGpio.digitalWrite(1)
+        }
+    }
+
+    this.reset = function() {
+        inGpio.digitalWrite(0)
+        outGpio.digitalWrite(0)
+    }
+}
+
+var components = {
+    'masterPower': new SingleControlPin(4),
+    'leftWinch': new DualControlPin(17, 27),
+    'rightWinch': new DualControlPin(22, 5),
+    'murray': new DualControlPin(14, 15)
+}
+
+
+app.get('/api/controls', function(req, res) {
+    try {
+        var controlName = req.query.control;
+        var state = req.query.state == 'true'
+
+        if (req.query.control == 'direction') {
+            for (let component in components) {
+                components[component].direction = state
+            }
+            console.log('Direction toggled to ' + (state ? 'OUT' : 'IN'))
+        } else {
+            console.log('MCP control %o setting to', controlName, state)
+            res.send(controlName + ': ' + state)
+            if (state) {
+                components[controlName].set()
+            } else {
+                components[controlName].reset()
+            }
+        }
+    } catch (err) {
+        console.log(err) //Gotta catch em all... Express handlers swallow all exceptions.
+    }
+})
+
+app.use(express.static('public'))
+
+for (let component in components) { //GPIO pins are initialized to HIGH if I remember correctly.
+    components[component].reset()
+}
 
 app.listen(3000, () => console.log('OttoDock Comm listening on port 3000'))
+
